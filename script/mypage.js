81ae3e294908407c736df64ef53e219569819a1e
chococo3827 <105043732+chococo3827@users.noreply.github.com>
2023-07-31 15:32:11 +0900

    Add files via upload

--- script/mypage.js
+++ script/mypage.js
@@ -1,64 +1,130 @@
 // mypage js
-// window.addEventListener("wheel", function(e){
-//     e.preventDefault();
-// },{passive : false});
 
-// const mHtml = $("html");
-// let page = 1;
+// 화면단위로 스크롤
+window.addEventListener("wheel", function(e){
+    e.preventDefault();
+},{passive : false});
 
-// mHtml.animate({scrollTop : 0},10);
+const mHtml = $("html");
+let page = 1;
 
-// $(window).on("wheel", function(e) {
-//     if(mHtml.is(":animated")) return;
-//     if(e.originalEvent.deltaY > 0) {
-//         if(page == 4) return;
-//         page++;
-//     } else if(e.originalEvent.deltaY < 0) {
-//         if(page == 1) return;
-//         page--;
-//     }
-//     let posTop =(page-1) * $(window).height();
-//     mHtml.animate({scrollTop : posTop});
-// })
+mHtml.animate({scrollTop : 0},10);
 
-const marquee1 = document.getElementById('first-marq')
-const marquee2 = document.getElementById('second-marq')
-
-const pTag1 = document.getElementById('first-parallel');
-const pTag2 = document.getElementById('second-parallel');
-
-const textArr1 = 'Hello Nice to meet you How are you I am fine Thak you'.split(' ');
-const textArr2 = 'This is my portfolio Please scroll down Have a good day'.split(' ');
-
-function initTexts(element, textArray){
-    textArray.push(...textArray)
-    for(let i = 0; i < textArray.length; i++){
-        element.innerText += `${textArray[i]}\u00A0\u00A0\u00A0\u00A0`
+$(window).on("wheel", function(e) {
+    if(mHtml.is(":animated")) return;
+    if(e.originalEvent.deltaY > 0) {
+        if(page == 4) return;
+        page++;
+    } else if(e.originalEvent.deltaY < 0) {
+        if(page == 1) return;
+        page--;
     }
-}
+    let posTop =(page-1) * $(window).height();
+    mHtml.animate({scrollTop : posTop});
+})
 
-initTexts(pTag1, textArr1);
-initTexts(pTag2, textArr2);
+// 스크롤 시 애니메이셔 효과
+$(window).scroll(function(){
+    const sct = $(this).scrollTop();
 
-let count1 = 0;
-let count2 = 0;
+    $("#aboutMe_info").each(function(){
+        const itemOst = $(this).offset().top - 500;
+        const itemEffect = $(this).attr('data-effect');
+        if(sct >= itemOst){
+            $(this).addClass(itemEffect);
+        }
+    })
 
-function marqueeText(count, element, direction){
-    if(count > element.scrollWidth / 2){
-        element.style.transform = `translate(0)`;
-        count = 0;
-    }
-    element.style.transform = `translate(${count * direction}px)`;
-    return count;
-}
+    $('#html').each(function(){
+        const itemOst = $(this).offset().top - 500;
+        const itemEffect = $(this).attr('data-effect');
+        if(sct >= itemOst){
+            $(this).addClass(itemEffect);
+        }
+    })
+    $('#css').each(function(){
+        const itemOst = $(this).offset().top - 500;
+        const itemEffect = $(this).attr('data-effect');
+        if(sct >= itemOst){
+            $(this).addClass(itemEffect);
+        }
+    })
+    $('#javascript').each(function(){
+        const itemOst = $(this).offset().top - 500;
+        const itemEffect = $(this).attr('data-effect');
+        if(sct >= itemOst){
+            $(this).addClass(itemEffect);
+        }
+    })
+    $('#jquery').each(function(){
+        const itemOst = $(this).offset().top - 500;
+        const itemEffect = $(this).attr('data-effect');
+        if(sct >= itemOst){
+            $(this).addClass(itemEffect);
+        }
+    })
+    $('#github').each(function(){
+        const itemOst = $(this).offset().top - 500;
+        const itemEffect = $(this).attr('data-effect');
+        if(sct >= itemOst){
+            $(this).addClass(itemEffect);
+        }
+    })
+})
 
-function animate(){
-    count1++;
-    count2++;
+$(window).scroll(function(){
+    $('#first').each(function(){
+        let bottom_of_element = $(this).offset().top + $(this).outerHeight();
+        let bottom_of_window = $(window).scrollTop() + $(window).height();
 
-    count1 = marqueeText(count1, marquee1, -1)
-    count2 = marqueeText(count2, marquee2, 1)
+        if(bottom_of_window > bottom_of_element){
+            $(this).addClass('active');
+        }
+    })
+})
 
-    window.requestAnimationFrame(animate);
-}
-animate();
+// header hover 효과
+$(function(){
+    $('header nav ul li a').hover(
+        function(){
+            $(this).addClass('active');
+        },
+        function(){
+            $(this).removeClass('active');
+        }
+    )
+})
+
+// 숫자 카운트 애니메이션
+$(window).scroll(function(){
+    $('.count-num').each(function() { // .count-num에 각각 적용
+        let bottom_of_element = $(this).offset().top + $(this).outerHeight();
+        let bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        if(bottom_of_window > bottom_of_element){
+            var $this = $(this),
+            countTo = $this.attr('data-count');
+            // $this = 첫번째~세번째 .count-num
+            // countTo = 첫번째~세번째 .count-num의 data-count 속성의 값(777,555,333)
+            
+            $({ countNum: $this.text()}).animate({
+                countNum: countTo 
+                // countNum: $this.text() = 0, countNum: countTo = 777, 555, 333
+                // 0에서 countNum이 된다
+            },
+            {
+                duration: 5000, // 애니메이션이 완료될때까지 걸리는 시간
+                easing:'linear', // 애니메이션 효과 방식
+                step: function() { // 움직임 각 스텝별로 실행될 함수
+                    $this.text(Math.floor(this.countNum));
+                    // Math.floor -> this.countNum의 값을 정수로 만들어준다
+                },
+                complete: function() { // 움직임이 멈춘 후 실행될 함수
+                    $this.text(this.countNum);
+                    // this.countNum이 $this의 text값이 된다
+                    //alert('finished');
+                }
+            });  
+        }
+      });
+})
